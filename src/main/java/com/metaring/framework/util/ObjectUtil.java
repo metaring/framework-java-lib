5f19ad020d7f92c93828b80f837cb11ae6c8da0b
Marco Vasapollo <ceo@metaring.com>
2019-08-20 18:11:20 +0200

    [RELEASE] DataRepresentation to Properties

--- src/main/java/com/metaring/framework/util/ObjectUtil.java
+++ src/main/java/com/metaring/framework/util/ObjectUtil.java
@@ -1,12 +1,12 @@
 /**
  *    Copyright 2019 MetaRing s.r.l.
- * 
+ *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
- * 
+ *
  *        http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
  *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,6 +18,7 @@ package com.metaring.framework.util;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.Properties;
 
 import org.apache.calcite.linq4j.Linq4j;
 
@@ -39,5 +40,27 @@ public final class ObjectUtil {
 
     public static final boolean isNullOrEmpty(DataRepresentation input) {
         return Objects.isNull(input) || input.isNullOrEmpty();
+    }
+
+
+    public static final Properties toProperties(final DataRepresentation rep) {
+        return toProperties(rep, null, null);
+    }
+
+    private static final Properties toProperties(final DataRepresentation initialData, Properties properties, String current) {
+        properties = properties == null ? new Properties() : properties;
+        if(ObjectUtil.isNullOrEmpty(initialData)) {
+            return properties;
+        }
+        for (String currentProperty : initialData.getProperties()) {
+            String propertyName = String.join(StringUtil.isNullOrEmpty(current) ? "" : ".", StringUtil.isNullOrEmpty(current) ? "" : current, currentProperty);
+            DataRepresentation dataRepresentation = initialData.get(currentProperty);
+            if (dataRepresentation.hasProperties()) {
+                toProperties(dataRepresentation, properties, propertyName);
+            } else {
+                properties.put(propertyName, initialData.getText(currentProperty));
+            }
+        }
+        return properties;
     }
 }
